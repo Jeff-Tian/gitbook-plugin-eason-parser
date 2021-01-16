211b2a6295d45a598c1d8e03c28da95d3887cb2c
Jeff Tian <jeff.tian@outlook.com>
2021-01-16 14:11:24 +0800

    fix: define

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ module.exports = {
   blocks: {
     eason: {
       process: function(block) {
+        const define = new SchemeParser.buildSyntaxTree(`(define (A x y)
+(cond ((= y 0) 0)
+      ((= x 0) (* 2 y))
+      ((= y 1) 2)
+      (else (A (- x 1)
+               (A x (- y 1))))))`)
+
+        define.define()
+        define.defineExplain()
+
         return '<pre>' + [
           new SchemeParser().buildSyntaxTree(block.body)
               .expandToEnd()].join('\n') + '</pre>'
